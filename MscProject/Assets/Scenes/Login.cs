b62a23690b5faa96b30be8305745c19753be0254
Zhu <313579822@qq.com>
2018-06-28 21:05:48 +0800

    a3

--- MscProject/Assets/Scenes/Login.cs
+++ MscProject/Assets/Scenes/Login.cs
@@ -89,5 +89,104 @@ public class Login : MonoBehaviour
         client.ClearMessageQueues();
         return responseTask;
     }
+    public enum MoveDirections
+    {
+        E, W, S, N, Se, Sw, Ne, Nw, SyntaxError
+    }
+    public ProtoFief Move(MoveDirections directions)
+    {
+        ProtoTravelTo protoTravel = new ProtoTravelTo();
+        protoTravel.travelVia = new[] { directions.ToString() };
+        protoTravel.characterID = "Char_158";
+        tclient.net.Send(protoTravel);
+        var reply = GetActionReply(Actions.TravelTo, tclient);
+        var travel = (ProtoFief)reply;
+        return travel;
+    }
+    public string[] getFiefID()
+    {
+        string[] IDs = new string[8];
+        IDs[0] = Move(MoveDirections.E).fiefID;
+        Move(MoveDirections.W);
+        IDs[1] = Move(MoveDirections.W).fiefID;
+        Move(MoveDirections.E);
+        IDs[2] = Move(MoveDirections.S).fiefID;
+        Move(MoveDirections.N);
+        IDs[3] = Move(MoveDirections.N).fiefID;
+        Move(MoveDirections.S);
+        IDs[4] = Move(MoveDirections.Se).fiefID;
+        Move(MoveDirections.Nw);
+        IDs[5] = Move(MoveDirections.Sw).fiefID;
+        Move(MoveDirections.Ne);
+        IDs[6] = Move(MoveDirections.Ne).fiefID;
+        Move(MoveDirections.Sw);
+        IDs[7] = Move(MoveDirections.Nw).fiefID;
+        Move(MoveDirections.Se);
+        return IDs;
+    }
+    public void iniFiefSence()
+    {
+        string[] IDs = getFiefID();
+       // mID.text = IDs[].text;
+        nID.text = IDs[3];
+        wID.text = IDs[1];
+        sID.text = IDs[2];
+        eID.text = IDs[0];
+        nwID.text = IDs[7];
+        neID.text = IDs[6];
+        swID.text = IDs[5];
+        seID.text = IDs[4];
+        //mB.onClick.AddListener(Move(MoveDirections.));
+        nB.onClick.AddListener(MoveButtonListenerN);
+        wB.onClick.AddListener(MoveButtonListenerW);
+        sB.onClick.AddListener(MoveButtonListenerS);
+        eB.onClick.AddListener(MoveButtonListenerE);
+        nwB.onClick.AddListener(MoveButtonListenerNW);
+        neB.onClick.AddListener(MoveButtonListenerNE);
+        swB.onClick.AddListener(MoveButtonListenerSW);
+        seB.onClick.AddListener(MoveButtonListenerSE);
+    }
+    
+    void MoveTo(MoveDirections directions)
+    {
+        Move(directions);
+        getFiefID();
+    }
+    void MoveButtonListenerE()
+    {
+        MoveTo(MoveDirections.E);
+    }
+    void MoveButtonListenerW()
+    {
+        MoveTo(MoveDirections.W);
+    }
+    void MoveButtonListenerS()
+    {
+        MoveTo(MoveDirections.S);
+    }
+    void MoveButtonListenerN()
+    {
+        MoveTo(MoveDirections.N);
+    }
+    void MoveButtonListenerSW()
+    {
+        MoveTo(MoveDirections.Sw);
+    }
+    void MoveButtonListenerNE()
+    {
+        MoveTo(MoveDirections.Ne);
+    }
+    void MoveButtonListenerNW()
+    {
+        MoveTo(MoveDirections.Nw);
+    }
+    void MoveButtonListenerSE()
+    {
+        MoveTo(MoveDirections.Se);
+    }
+    void DetailButtonListener()
+    {
+
+    }
 
 }
